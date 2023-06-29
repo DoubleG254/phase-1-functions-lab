11da442ec2547a71566386b2bd865e28ec2e0926
DoubleG254 <glorygweth6@gmail.com>
2023-06-29 16:30:27 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+   let blocks = Math.max(distance, 42)-Math.min(distance, 42)
+  return  blocks
+}
+function distanceFromHqInFeet(distance){
+    let feet =  (Math.max(distance, 42)-Math.min(distance, 42)) * 264
+     return feet
+}
+function distanceTravelledInFeet(start, destination){
+    let feet = (Math.max(start, destination)-Math.min(start, destination)) * 264
+    return feet
+}
+function calculatesFarePrice(start, destination){
+    let feet = (Math.max(start, destination)-Math.min(start, destination)) * 264
+    if(feet<=400){
+        return 0
+    }
+    if(feet>400 && feet<=2000){
+        return (feet-400) * 0.02
+    }
+    if(feet>2000 && feet<=2500){
+        return 25
+    }
+    if(feet>2500){
+        return `cannot travel that far`
+    }
+
+}
